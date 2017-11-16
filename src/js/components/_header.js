94836274cadec103fffe7d92b01a3c1604cd2810
whats0n <mrdvv@mail.ru>
2017-11-16 03:35:38 +0200

    update function: toggle header on scroll and load

--- src/js/components/_header.js
+++ src/js/components/_header.js
@@ -4,61 +4,62 @@ import { WIN, FIXED } from '../_constants';
 
   const $header = $('.js-header');
   const duration = 200;
-  const easing = 'easeinout';
   let shown = false;
   let hidden = false;
-  let counter = 0;
 
-  WIN.on('scroll', e => {
-		
+  const setHeader = position => $header.css({
+    '-webkit-transform': `translate3d(0,${position-100}%,0)`,
+    'transform': `translate3d(0,${position-100}%,0)`
+  });
+  
+  const toggleHeader = () => {
+    
     const scrollTop = WIN.scrollTop();
-    const height = $header.outerHeight() * 2;
+    const distance = $header.outerHeight() * 2;
+    const conditionDistance = scrollTop > distance;
 
-    if (scrollTop > height && !shown) {
-    	shown = true;
-    	$header
-    		.css({
-    			'-webkit-transform': 'translate3d(0,-100%,0)',
-    			'transform': 'translate3d(0,-100%,0)'
-    		})
-    		.addClass(FIXED)
-    		.stop(true,true,true)
-    		.animate({
-    			counter: 100
-    		}, {
-    			duration: duration,
-    			step: now => $header.css({
-    				'-webkit-transform': `translate3d(0,${now-100}%,0)`,
-    				'transform': `translate3d(0,${now-100}%,0)`
-    			}),
-    			complete: () => {
-    				hidden = false;
-    			}
-    		});
-    } else if (scrollTop <= height && shown && !hidden) {
-    	hidden = true;
-    	$header
-    		.stop(true,true,true)
-    		.animate({
-    			counter: 0
-    		}, {
-    			duration: duration,
-    			step: now => $header.css({
-    				'-webkit-transform': `translate3d(0,${now-100}%,0)`,
-    				'transform': `translate3d(0,${now-100}%,0)`
-    			}),
-    			complete: () => {
-			    	shown = false;
-    				$header
-			    		.css({
-			    			'-webkit-transform': 'translate3d(0,0,0)',
-			    			'transform': 'translate3d(0,0,0)'
-			    		})
-    					.removeClass(FIXED);
-    			}
-    		});
+    if (conditionDistance && !shown) {
+      shown = true;
+      $header
+        .css({
+          '-webkit-transform': 'translate3d(0,-100%,0)',
+          'transform': 'translate3d(0,-100%,0)'
+        })
+        .addClass(FIXED)
+        .stop(true,true,true)
+        .animate({
+          counter: 100
+        }, {
+          duration: duration,
+          step: now => setHeader(now),
+          complete: () => {
+            hidden = false;
+          }
+        });
+    } else if (!conditionDistance && shown && !hidden) {
+      hidden = true;
+      $header
+        .stop(true,true,true)
+        .animate({
+          counter: 0
+        }, {
+          duration: duration,
+          step: now => setHeader(now),
+          complete: () => {
+            shown = false;
+            $header
+              .css({
+                '-webkit-transform': 'translate3d(0,0,0)',
+                'transform': 'translate3d(0,0,0)'
+              })
+              .removeClass(FIXED);
+          }
+        });
     }
 
-  });
+  };
+
+  toggleHeader();
+  WIN.on('scroll', toggleHeader);
 
 })();
