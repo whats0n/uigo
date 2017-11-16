10afcf17b5d8ae1dbb8faf989b4888ef26456639
whats0n <mrdvv@mail.ru>
2017-11-16 16:23:18 +0200

    add sm-info section

--- src/js/components/_scrollto.js
+++ src/js/components/_scrollto.js
@@ -4,6 +4,7 @@ import { SCROLL_TO } from '../_utils';
 
   const $goto = $('[data-goto]');
   const $section = $('[data-section]');
+  const $header = $('.js-header');
 
   $goto.on('click', e => {
 
@@ -13,7 +14,7 @@ import { SCROLL_TO } from '../_utils';
     const to = $target.data('goto');
     const $currentSection = $section.filter(`[data-section="${to}"]`);
     
-    $currentSection.length && SCROLL_TO($currentSection.offset().top);
+    $currentSection.length && SCROLL_TO($currentSection.offset().top - $header.outerHeight());
 
   });
 
